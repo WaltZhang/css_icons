b3c72a725f0a93035d305212fb58bc569c45610e
张文广 <zhangwenguang@junzhengloan.com>
2019-11-01 15:54:13 +0800

    first 6 icons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <home />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
+<script>
+import Home from "./views/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div><circle-loading /></div>
+    <div><bubble-loading /></div>
+    <div><teacup-loading /></div>
+    <div><play-button /></div>
+    <div><floating-button /></div>
+    <div><progress-bar /></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CircleLoading from "@/components/CircleLoading.vue";
+import BubbleLoading from "@/components/BubbleLoading.vue";
+import TeacupLoading from "@/components/TeacupLoading.vue";
+import PlayButton from "@/components/PlayButton.vue";
+import FloatingButton from "@/components/FloatingButton.vue";
+import ProgressBar from "@/components/ProgressBar.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    CircleLoading,
+    BubbleLoading,
+    TeacupLoading,
+    PlayButton,
+    FloatingButton,
+    ProgressBar,
   }
-}
+};
 </script>
+
+<style>
+.home {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  justify-items: center;
+  align-items: center;
+  max-width: 800px;
+  height: 100vh;
+  margin: 0 auto;
+}
+
+.home > div {
+  transform: scale(1.5);
+}
+</style>
